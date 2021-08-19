d5b4ce6bef77181da03707ba0e7c769608e6a418
SergeyNacc <sergeynaksin@gmail.com>
2021-08-19 18:19:37 +0300

    added the logic for changing the text

--- app/src/main/java/nacc/sergey/textquest/IntroFragment.kt
+++ app/src/main/java/nacc/sergey/textquest/IntroFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_intro.*
 import nacc.sergey.textquest.databinding.FragmentIntroBinding
 
 
@@ -12,8 +13,12 @@ class IntroFragment : Fragment() {
 
     private lateinit var binding: FragmentIntroBinding
 
-    private val textIntroList = listOf(R.string.intro2, R.string.intro3, R.string.intro4,
-        R.string.intro5, R.string.intro6, R.string.intro7, R.string.intro8, R.string.intro9)
+    private val textIntroList = listOf( (R.string.intro1), (R.string.intro2),
+            (R.string.intro3), (R.string.intro4), (R.string.intro5),
+            (R.string.intro6), (R.string.intro7), (R.string.intro8),
+            (R.string.intro9))
+
+    private var currentIndex = 0
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,13 +29,20 @@ class IntroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.textPlot.text = getText(R.string.intro1)
+        updateText()
 
         binding.buttonNext.setOnClickListener {
-            textIntroList.forEach()
+            currentIndex = (currentIndex + 1) % textIntroList.size
+            updateText()
+
+            // как только массив заканчивается,
+            // нужен код для вызова следующего фрагмента
         }
+    }
 
-
+    private fun updateText() {
+        val textId = textIntroList[currentIndex]
+        binding.textPlot.setText(textId)
     }
 
 }
